c5bb06e4772f52b3f00a92a968b1fc2bcf07dcd5
ayaxsantos <ayax.asantos@gmail.com>
2022-11-11 18:23:36 -0300

    Controlador PI Func - Primer aproximacion control proporcional

--- controlador-pi-func/app/Main.hs
+++ controlador-pi-func/app/Main.hs
@@ -2,7 +2,9 @@ module Main (main) where
 
 {-# LANGUAGE RebindableSyntax #-}
  
+import Prelude hiding ((<=),(&&),(>=))
 import Copilot.Arduino.Uno
+import Copilot.Arduino.Internals as Internals
 import qualified Copilot.Arduino.Library.Serial as Serial
 
 baudRateComm :: Int
@@ -21,11 +23,26 @@ leerValorAnalogico unaEntrada = do
     unaLectura <- input unaEntrada :: Sketch (Behavior ADC)
     return unaLectura
 
-mostrarLecturaSerial :: String -> Behavior Float -> Sketch ()
+mostrarLecturaSerial :: (Internals.ShowCType a, Typed a) => String -> Behavior a -> Sketch ()
 mostrarLecturaSerial unTexto unaLectura = do
     Serial.baud baudRateComm
     let msg = [Serial.str unTexto, Serial.show unaLectura, Serial.str "\n"]
     Serial.device =: msg
+
+unaCondicion :: Behavior ADC -> Behavior Bool
+unaCondicion unError = unError >= 0 && unError <= 255
+
+deWord16HaciaWord8 :: Behavior Word16 -> Behavior Word8
+deWord16HaciaWord8 unValor = unsafeCast unValor
+
+-- 1023/255 aprox = 4
+mapearValor :: Behavior ADC -> Behavior Word8
+mapearValor unValor = (*4).deWord16HaciaWord8.adaptarValor $ unValor
+        where adaptarValor :: Behavior Int16 -> Behavior Word16
+              adaptarValor unValorPri = unsafeCast unValorPri
+
+controlProporcional :: Behavior Word8 -> Behavior Word8
+controlProporcional unError = kp * unError 
 
 main :: IO ()
 main = arduino $ do 
@@ -38,6 +55,9 @@ main = arduino $ do
     let valorDeError = valorDeReferencia - lecturaTemperatura
 
     (mostrarLecturaSerial "Temperatura leida: ").equivalenciaGradosDe $ lecturaTemperatura
-    (mostrarLecturaSerial "Valor del error: " ).equivalenciaGradosDe $ valorDeReferencia
+    (mostrarLecturaSerial "Valor de referencia: " ).equivalenciaGradosDe $ valorDeReferencia
+    mostrarLecturaSerial "Valor PWM: " valorDeError    
+
+    pin5 =: (pwm . controlProporcional . mapearValor $ valorDeError)
 
     delay =: MilliSeconds (constant 1000)
